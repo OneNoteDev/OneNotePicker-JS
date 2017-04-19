07f804cc155fabb15e5a0c0a51e991aa04b644ca
Anbiniyar Muniandy <anbiniyar@gmail.com>
2017-04-19 11:26:56 -0700

    A simple test case that can report code coverage

---
+++ src/simple.ts
@@ -0,0 +1,7 @@
+export default class Simple {
+	public age: number;
+
+	greet() {
+		console.log(this.age);
+	}
+}
--- test/sample.spec.ts
+++ test/sample.spec.ts
@@ -1,8 +1,18 @@
-import OneNoteSectionPicker from "../src/oneNoteSectionPicker";
 import {expect} from "chai";
+import OneNoteSectionPicker from "../src/oneNoteSectionPicker";
+import Simple from "../src/simple";
 
 describe("TopLevel", () => {
 	it("should work", () => {
 		let picker = new OneNoteSectionPicker();
+		//noinspection TsLint
+		expect(picker).to.exist;
+	});
+
+	it("should get coverage", () => {
+		let coverage = new Simple();
+		coverage.age = 10;
+		coverage.greet();
+		expect(coverage.age).to.equal(10);
 	});
 });
