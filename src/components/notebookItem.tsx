90fbe2931a4c1563e3e3f3f77f9893708ebac144
Chad Shurtz <chadsh@microsoft.com>
2017-05-03 15:31:11 -0700

    Add basic expand/collapse

--- src/components/notebookItem.tsx
+++ src/components/notebookItem.tsx
@@ -2,12 +2,19 @@ import * as React from 'react';
 import SectionItem from './sectionItem';
 import SectionGroupItem from './sectionGroupItem';
 
-class NotebookItem extends React.Component<{notebook: OneNoteApi.Notebook}, null> {
+class NotebookItem extends React.Component<{ notebook: OneNoteApi.Notebook }, any> {
+	constructor(props) {
+		super(props);
+		this.state = { expanded: false };
+	}
+
 	render() {
+		let { expanded } = this.state;
+
 		return (
 			<li>
-				<a className={'is-active'}>{this.props.notebook.name}</a>
-				<ul>
+				<a onClick={() => this.setState({ expanded: !expanded })}>{this.props.notebook.name}</a>
+				<ul style={this.state.expanded ? { display: 'block' } : { display: 'none' }}>
 					{this.props.notebook.sectionGroups.map(sectionGroup => <SectionGroupItem sectionGroup={sectionGroup} key={sectionGroup.name}></SectionGroupItem>)}
 					{this.props.notebook.sections.map(section => <SectionItem section={section} key={section.name}></SectionItem>)}
 				</ul>
