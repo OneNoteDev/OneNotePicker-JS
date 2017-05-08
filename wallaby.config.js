0ee385c344e077a08f25f18a5651462e4bcb4222
Anbiniyar Muniandy <anbiniyar@gmail.com>
2017-05-07 22:19:46 -0700

    Add WallabyJS Support

---
+++ wallaby.config.js
@@ -0,0 +1,29 @@
+let wallabyWebpack = require('wallaby-webpack');
+
+let webpackConfig = {};
+webpackConfig.externals = webpackConfig.externals || {};
+webpackConfig.externals['react/lib/ExecutionEnvironment'] = true;
+webpackConfig.externals['react/lib/ReactContext'] = true;
+webpackConfig.externals['react/addons'] = true;
+
+let wallabyPostprocessor = wallabyWebpack(webpackConfig);
+
+module.exports = function (wallaby) {
+	return {
+		testFramework: 'jasmine',
+
+		files: [
+			{pattern: 'src/**/*.tsx', load: false},
+		],
+		tests: [
+			{pattern: 'spec/**/*.tsx', load: false}
+		],
+
+		postprocessor: wallabyPostprocessor,
+
+		setup: function () {
+			// required to trigger test loading
+			window.__moduleBundler.loadTests();
+		}
+	};
+};
