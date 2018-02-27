6a6b418c4e363a0b048f62d02615ce404863ef8c
Anbiniyar Muniandy <anbinm@microsoft.com>
2018-02-27 12:16:45 -0800

    Extract section icon into its own component

---
+++ src/components/icons/sectionIcon.svg.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class SectionIconSvg extends React.Component {
+	render() {
+		return (
+			<svg className='section-icon' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' viewBox='0 0 20 20'>
+				<path d='M12,3H6v14h6v2h1V1h-1V3z'/>
+			</svg>
+		);
+	}
+}
