72c01d0b3fcd59cc04dc6166546c04f721fddac5
Anbiniyar Muniandy <anbinm@microsoft.com>
2018-03-15 11:11:47 -0700

    Use currentColor in SVG
    To make it easier to style with SVG, we will use currentColor. This way
    we can simply set color property and the SVG will follow suit.

--- src/components/icons/notebookOpenedIcon.svg.tsx
+++ src/components/icons/notebookOpenedIcon.svg.tsx
@@ -4,7 +4,7 @@ export class NotebookOpenedIconSvg extends React.Component {
 	render() {
 		return (
 			<svg className='notebook-opened-icon' viewBox='0 0 16 14' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
-				<g id='icon-container' stroke='none' strokeWidth='1' fill='#444444' fillRule='evenodd'>
+				<g id='icon-container' stroke='none' strokeWidth='1' fill='currentColor' fillRule='evenodd'>
 					<path d='M14.5,1 C15.053,1 15.5,1.447 15.5,2 L15.5,13 C15.5,13.553 15.053,14 14.5,14 L1.5,14 C0.947,14 0.5,13.553 0.5,13 L0.5,2 C0.5,1.447 0.947,1 1.5,1 L1.5,0 C3.26,0 5.731,0.373 7.186,1 L8.814,1 C10.269,0.373 12.74,0 14.5,0 L14.5,1 Z M7.5,13 L7.5,2.5 C6.57,1.76 4.35,1.06 2.5,1 L2.5,11.5 C4.28,11.55 6.43,12.31 7.5,13 Z M13.5,11.5 L13.5,1 C11.33,1.06 9.59,1.76 8.5,2.5 L8.5,13 C9.76,12.31 11.41,11.55 13.5,11.5 Z' />
 				</g>
 			</svg>
--- src/components/icons/spinnerIcon.svg.tsx
+++ src/components/icons/spinnerIcon.svg.tsx
@@ -5,8 +5,8 @@ export class SpinnerIconSvg extends React.Component {
 		return (
 			<svg className='spinner' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
 				<g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
-					<circle className='spinner-circle' stroke='#C7E0F4' strokeWidth='5' cx='32' cy='32' r='29'/>
-					<path className='spinner-progress' stroke='#0078D7' strokeWidth='5' d='M52.6861723,11.5097602 C47.418631,6.25144806 40.1470894,3 32.1161315,3 C24.0850974,3 16.8134942,6.25150964 11.545941,11.5099096'/>
+					<circle className='spinner-circle' stroke='currentColor' strokeWidth='5' cx='32' cy='32' r='29'/>
+					<path className='spinner-progress' stroke='currentColor' strokeWidth='5' d='M52.6861723,11.5097602 C47.418631,6.25144806 40.1470894,3 32.1161315,3 C24.0850974,3 16.8134942,6.25150964 11.545941,11.5099096'/>
 				</g>
 			</svg>
 		);
