5c9799d53647d49fcd5473ec558d111300dddddb
anbinm-msft <anbinm@microsoft.com>
2018-03-15 14:41:15 -0700

    Use currentColor in SVG (#102)
    To make it easier to style with SVG, we will use currentColor. This way
    we can simply set color property and the SVG will follow suit.

--- src/components/icons/chevron.svg.tsx
+++ src/components/icons/chevron.svg.tsx
@@ -5,7 +5,7 @@ export class ChevronSvg extends React.Component {
 		return (
 			<svg className='chevron-icon' viewBox='0 0 15 8' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
 				<g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
-					<g fill='#666666'>
+					<g fill='currentColor'>
 						<polygon id='Triangle' points='7.5 0 15 8 0 8'/>
 					</g>
 				</g>
